67ee933b493773c656edc7233e088ecde6be1f2f
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-06-30 13:00:33 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+
+var random = Math.ceil(Math.random() * 10) + 1;
+
+var num3 = 10;
+var num4 = 6;
+var mod = num3 % num4;
+
+var max = Math.max(1, 12, 6, 4, 20);
+
